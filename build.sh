72e4af6ba6c8f716b2ad0adb0136e63c4ac90c18
Prashant <prashant33968@gmail.com>
2022-07-14 08:34:34 +0530

    Let's build SuperiorOS

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Normal build steps
-lunch rom_lavender-userdebug
+export BUILD_WITH_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+lunch superior_lavender-userdebug
 make bacon -j$(nproc --all)
