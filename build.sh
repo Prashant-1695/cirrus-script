87281d6fbba4e0bd6941ebd3299b67f78ad6aa84
Prashant <prashant33968@gmail.com>
2022-07-28 19:35:29 +0530

    Build | V

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 # Normal build steps
 . build/envsetup.sh
-lunch rom_lavender-userdebug
+lunch lineage_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
+export KBUILD_BUILD_USER=Prashant
+export KBUILD_BUILD_HOST=Ubuntu
+export BUILD_USERNAME=ImPrashantt
+export BUILD_HOSTNAME=Ubuntu
 
 compile_plox () {
 make bacon -j$(nproc --all)
